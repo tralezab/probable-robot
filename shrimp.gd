072737b768502cdb4c556fb856461882f53b0d76
tralezab <spamqetuo2@gmail.com>
2020-05-16 12:42:42 -0700

    attack code

--- shrimp.gd
+++ shrimp.gd
@@ -5,6 +5,12 @@ const SPEED = 150
 const MAX_HEALTH = 100
 var health = 100
 
+const ATTACK_COOLDOWN = 50
+var attack_timer = 0
+var attack_damage = 50
+onready var attacksprite = $Attack
+onready var attackarea = $Attack_Area
+
 onready var destination = get_parent().get_node("Destination")
 
 # Called when the node enters the scene tree for the first time.
@@ -12,10 +18,11 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
+	attack_timer -= 1
 	if !current:
 		return
 	var direction_to = destination.global_position - global_position
-	if destination.global_position != global_position:
+	if destination.visible != false:
 		rotation = direction_to.angle()
 		if abs(rad2deg(rotation)) > 90:
 			$Sprite.flip_v = true
@@ -38,8 +45,22 @@ func _process(delta):
 	if Input.is_action_pressed("rightclick"):
 		destination.global_position = get_global_mouse_position()
 		destination.visible = true
-	if Input.is_action_just_pressed("leftclick"):
-		print("attack")
+	if Input.is_action_just_pressed("leftclick") and attack_timer <= 0:
+		attack_move()
+
+func attack_move():
+	attack_timer = ATTACK_COOLDOWN
+	attacksprite.visible = true
+	var attacked = attackarea.get_overlapping_bodies()
+	for shramp in attacked:
+		if shramp == attackarea.get_parent():
+			continue
+		if shramp is RigidBody2D:
+			shramp.health -= attack_damage
+			if shramp.health <= 0:
+				shramp.queue_free()
+	yield(get_tree().create_timer(0.5), "timeout")
+	attacksprite.visible = false
 
 func set_current(boo):
 	current = boo
