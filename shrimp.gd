201f259fc03d362b7b6a76d8df056c5c74430048
tralezab <spamqetuo2@gmail.com>
2020-05-15 22:27:39 -0700

    more movement + current
    current is a temporary system for testing attacks

--- shrimp.gd
+++ shrimp.gd
@@ -1,9 +1,9 @@
 extends RigidBody2D
 
-
-const SPEED = 100
-const MAX_SPEED = 200
-
+export var current = false
+const SPEED = 150
+const MAX_HEALTH = 100
+var health = 100
 
 onready var destination = get_parent().get_node("Destination")
 
@@ -12,16 +12,35 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
+	if !current:
+		return
 	var direction_to = destination.global_position - global_position
-	rotation = direction_to.angle()
-	if abs(rad2deg(rotation)) > 90:
-		$Sprite.flip_v = true
+	if destination.global_position != global_position:
+		rotation = direction_to.angle()
+		if abs(rad2deg(rotation)) > 90:
+			$Sprite.flip_v = true
+		else:
+			$Sprite.flip_v = false
+			
+		##REMINDER: SET THE SHRIMP TO SLOWLY LOSE MOMENTUM FOR COLLISIONS
+			
+		#linear_velocity = 
+	if direction_to.length() < 10:
+		linear_velocity = Vector2.ZERO
+		destination.global_position = global_position
+		destination.visible = false
+		pass
 	else:
-		$Sprite.flip_v = false
-	linear_velocity = (direction_to * SPEED * delta)
-	
+		linear_velocity = direction_to.clamped(SPEED) * SPEED * delta
 
 func _process(delta):
+	if !current:
+		return
 	if Input.is_action_pressed("rightclick"):
 		destination.global_position = get_global_mouse_position()
-	pass
+		destination.visible = true
+	if Input.is_action_just_pressed("leftclick"):
+		print("attack")
+
+func set_current(boo):
+	current = boo
