b4ce779f833d308ab2395f453f401958235b0389
tralezab <spamqetuo2@gmail.com>
2020-05-23 19:09:28 -0700

    gonna be yuge changes

--- Menu.gd
+++ Menu.gd
@@ -2,6 +2,8 @@ extends Node
 
 var tabs = null
 
+var class_dict = {"Basic":"res://testing_shrimp.tscn", "Glass Cannon":"res://one_shot_johnny.tscn"}
+
 func _ready():
 	tabs = get_node("Selection")
 	setup_tabs()
@@ -12,9 +14,24 @@ func setup_tabs():
 		var name = "cross"+str(cur+1)
 		cursors.add_item(name, load("res://shrimpicons/"+name+".png"))
 
+	var classes = tabs.get_node("Classes")
+	for type in range(len(class_dict)):
+		var name = class_dict.keys()[type]
+		classes.add_item(name, load("res://shrimpicons/"+name+".png"))
+
+func _on_respawn_pressed():
+	if gamestate.player_node.shrimp == null:
+		gamestate.player_node.spawn_shrimp()
+
 func _on_Button_pressed():
 	tabs.visible = not tabs.visible
 
 func _on_Cursors_item_selected(index):
 	var item = $Selection/Cursors.get_item_icon(index)
 	gamestate.player_node.set_cursor(item)
+
+#picking a class
+func _on_class_selected(index):
+	gamestate.player_node.set_shrimp(class_dict[class_dict.keys()[index]])
+
+
